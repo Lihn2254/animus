a99828a71558e6b9816a2f0e758fabc5ef3319c4
Lihn2254 <erick.davidhf@gmail.com>
2026-03-21 19:55:27 -0700

    Fix: Quitar ruta duplicada delete_account_route

--- backend/routes/account_routes.py
+++ backend/routes/account_routes.py
@@ -1,11 +1,10 @@
 from flask import Blueprint
-from controllers.account_controller import delete_account, update_account
 from middlewares.jwt_required import jwt_required
 from controllers.account_controller import (
     delete_account,
     delete_account_by_credentials,
     get_account,
-    update_account,
+    update_account
 )
 
 account_bp = Blueprint("account", __name__)
@@ -15,6 +14,7 @@ account_bp = Blueprint("account", __name__)
 @jwt_required
 def delete_account_route(user_id):
     return delete_account(user_id)
+
 @account_bp.route("/account/<int:user_id>", methods=["GET"])
 def get_account_route(user_id):
     return get_account(user_id)
@@ -24,12 +24,6 @@ def get_account_route(user_id):
 @jwt_required
 def update_account_route(user_id):
     return update_account(user_id)
-
-
-@account_bp.route("/account/<int:user_id>", methods=["DELETE"])
-def delete_account_route(user_id):
-    return delete_account(user_id)
-
 
 @account_bp.route("/account/delete", methods=["DELETE"])
 def delete_account_by_credentials_route():
